3133f3118783eeba447f8ad4f31bce955456cf38
ddovale <dovale.daniel07@gmail.com>
2020-11-24 18:49:45 -0300

    DP - Adapter

---
+++ JAVA/src/test/java/br/com/esportes/estudos/designpatterns/structural/adapter/SquarePeg.java
@@ -0,0 +1,19 @@
+package br.com.esportes.estudos.designpatterns.structural.adapter;
+
+public class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare() {
+        double result;
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
